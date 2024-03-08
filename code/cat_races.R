c260f9f8e92d5b06acaf540f6ff457bee22421c6
Piotr <piopac1@st.amu.edu.pl>
2024-03-08 10:30:53 +0100

    aktualizacja pliku z kodem do zajec 2, dodano brakujacy kod do agregacji i dodano opisy

--- code/cat_races.R
+++ code/cat_races.R
@@ -1,11 +1,13 @@
 library(dplyr)
 
-getwd()
-
-setwd('C:\\Users\\ppacula24\\Desktop\\c_ag2')
+# otworzyc plik z surowymi danymi
 
 blocks = read.csv('dane_atrybutowe\\blocks2010.csv')
 blocks = blocks[blocks$COUNTY_CODE==26163,]
+
+## kategoryzacja
+
+# odpowiednio dopasować kategore wg tego co na stronie dmowska-dydaktyka
 
 blocks$whites = blocks$H7Z003
 
@@ -19,6 +21,24 @@ blocks$others = blocks$H7Z008 + blocks$H7Z009
 
 blocks$latino = blocks$H7Z010
 
+# zmienic H7Z na poczatek nazwy kolumny dla odpowieniej daty
+
 blocks <- blocks %>% select(-starts_with("H7Z"))
 
-write.csv(blocks, 'dane_atrybutowe\\detroit2010.csv')
+# ustawic miejsce zapisania pliku
+
+write.csv(blocks, 'dane_atrybutowe\\wayne_2010.csv')
+
+## agregacja
+
+list_race <- c("whites", "blacks", "asians", "native_americans", "others", "latino")
+
+blocks2 = blocks
+
+blocks2$tot = rowSums(blocks2 %>% select(one_of(list_race)))
+
+blocks2 <- aggregate(.~GISJOIN_T, blocks2[, c("GISJOIN_T", list_race, "tot")], FUN=sum)
+
+# ustawic miejsce zapisania pliku
+
+write.csv(blocks2, 'dane_atrybutowe\\wayne_aggr_2010.csv')
